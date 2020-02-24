fbc04f9b955ac5fc1d300f14a3527161d2f925b3
Propietario <Propietario@Rafa>
2020-02-24 11:45:13 +0100

    Integramos el sistema JWT y creamos la parte de seguridad

---
+++ src/main/java/com/proyecto/thegiftcher/web/JwtAuthenticationController.java
@@ -0,0 +1,55 @@
+package com.proyecto.thegiftcher.web;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.thegiftcher.config.JwtTokenUtil;
+import com.proyecto.thegiftcher.domain.JwtRequest;
+import com.proyecto.thegiftcher.domain.JwtResponse;
+import com.proyecto.thegiftcher.service.JwtUserDetailsService;
+
+@RestController
+public class JwtAuthenticationController {
+	
+	private final static Logger LOGGER = Logger.getLogger("JwtAuthenticationController");
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	@Autowired
+	private JwtUserDetailsService jwtUserDetailsService;
+	/*
+	 * endpoint para que el usuario se autentique con su usuario y contraseña y obtenga el token
+	 */
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping(value = "/login")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		LOGGER.log(Level.INFO, "******** "+authenticationRequest.getUsername()+" "+authenticationRequest.getPassword());
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String token = jwtTokenUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+		
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
