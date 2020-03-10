e5088702f844ba84d3dc3d886cf0fd04eac2c4f6
Jorge García <jorge_garcia_daoa1819@cev.com>
2020-03-11 00:04:36 +0100

    Añadido /user a permitAll

--- src/main/java/com/proyecto/thegiftcher/config/WebSecurityConfig.java
+++ src/main/java/com/proyecto/thegiftcher/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/login", "/register-prueba", "/register")
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/login", "/register-prueba", "/register", "/user")
 				.permitAll().anyRequest().authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
