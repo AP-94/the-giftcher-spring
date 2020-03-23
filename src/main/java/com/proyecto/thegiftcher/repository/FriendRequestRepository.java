e20fa04f54f4ecd0954e99a49cfcab07bd479c7c
rajullaso <45672664+rajullaso@users.noreply.github.com>
2020-03-23 09:32:20 +0100

    Creación de acciones entre amistades

---
+++ src/main/java/com/proyecto/thegiftcher/repository/FriendRequestRepository.java
@@ -0,0 +1,12 @@
+package com.proyecto.thegiftcher.repository;
+
+import com.proyecto.thegiftcher.domain.FriendRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+    Iterable<FriendRequest> findAllFriendsRequestsByFriendRequestId(long friendRequestId);
+}
