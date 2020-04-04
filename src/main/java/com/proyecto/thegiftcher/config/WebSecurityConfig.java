a6edc94c9c1a5bb38dee29984ba82ade812fc071
Jorge García <jorge_garcia_daoa1819@cev.com>
2020-04-04 20:25:57 +0200

    Agregado a seguridad /user y cambiado .war a .jar para subir a AWS

--- src/main/java/com/proyecto/thegiftcher/config/WebSecurityConfig.java
+++ src/main/java/com/proyecto/thegiftcher/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/user/login", "/register-prueba", "/register", "/user")
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/user/login", "/user/register")
 				.permitAll().anyRequest().authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
